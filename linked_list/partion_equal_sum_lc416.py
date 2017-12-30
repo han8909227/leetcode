6921b9f7e4d83f08c25850de06c861cc34caba2a
han8909227 <hbao2016@hotmail.com>
2017-12-30 15:59:43 -0800

    added equal partion sum problem

---
+++ linked_list/partion_equal_sum_lc416.py
@@ -0,0 +1,35 @@
+
+# Given a non-empty array containing only positive integers,
+# find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+
+# Note:
+# Each of the array element will not exceed 100.
+# The array size will not exceed 200.
+# Example 1:
+# Input: [1, 5, 11, 5] or  [1, 2, 3, 4, 5, 5, 6, 7, 8, 9]
+# Output: true
+# Explanation: The array can be partitioned as [1, 5, 5] and [11].
+
+# Example 2:
+# Input: [1, 2, 3, 5]
+# Output: false
+# Explanation: The array cannot be partitioned into equal sum subsets.
+
+
+def partion_subset_sum(data):
+    """Return if equal subset exist."""
+    total = 0
+    for val in data:
+        total += val
+
+    if total % 2 == 1:  # if total cannot even divide, no equal subset
+        return False
+
+    aim = total / 2
+
+    boolean_set = set([0])  # keep track of sums
+    for val in data:
+        for s in boolean_set.copy():
+            boolean_set.add(s + val)
+    return aim in boolean_set
+

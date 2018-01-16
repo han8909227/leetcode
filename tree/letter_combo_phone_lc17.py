15c534b4d95f2c314fe2fc146016391304b96dfc
han8909227 <hbao2016@hotmail.com>
2018-01-15 22:31:01 -0800

    completed lc17

--- tree/letter_combo_phone_lc17.py
+++ tree/letter_combo_phone_lc17.py
@@ -8,3 +8,22 @@
 # Although the above answer is in lexicographical order, your answer could be in any order you want.
 
 
+def letter_combinations(self, digits):
+    """LC17 in Python."""
+    if not digits:
+        return []
+    dic = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
+    res = []
+    dfs(digits, dic, 0, "", res)
+    return res
+
+
+def dfs(self, digits, dic, index, path, res):
+    """DFS."""
+    if len(path) == len(digits):
+        res.append(path)
+        return
+    for i in xrange(index, len(digits)):
+        for j in dic[digits[i]]:
+            dfs(digits, dic, i + 1, path + j, res)
+

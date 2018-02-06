850c8af888471c6d9a5e9668a4d88776e9930c57
han8909227 <hbao2016@hotmail.com>
2018-02-06 14:38:54 -0800

    added simple binary search function

--- dynamic_programming/unique_path_ii_lc63.py
+++ dynamic_programming/unique_path_ii_lc63.py
@@ -17,3 +17,28 @@
 # Note: m and n will be at most 100.
 
 
+def unique_path_ii(nums):
+    """LC63 Dynamic programming."""
+    if nums[0][0] == 1:  # cannot start
+        return 0
+    m = len(nums)
+    n = len(nums[0])
+    dp = [[0 for _ in range(n)] for _ in range(m)]
+    dp[0][0] = 1  # set the start point
+    for i in range(1, m):  # change edges since path traversal starts 1,1
+        dp[i][0] = dp[i - 1][0] if nums[i][0] == 0 else 0
+    for j in range(1, n):  # change edges since path traversal starts 1,1
+        dp[0][j] = dp[0][j - 1] if nums[0][j] == 0 else 0
+
+    for i in range(m):  # path traversal
+        for j in range(n):
+            if nums[i][j] == 1:  # if cannot be passed, erase that possible route
+                dp[i][j] = 0
+            else:  # if can be passed, accumualte from previous(from top and left)
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+    return dp[m - 1][n - 1]  # the total number of routes
+
+
+if __name__ == '__main__':
+    m = [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
+
